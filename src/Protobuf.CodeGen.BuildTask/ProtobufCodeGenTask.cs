fc35af9cfb7449319036f70d1d5260c12c124ebe
hanabi1224 <harlowmoo@gmail.com>
2019-01-16 12:55:36 +0800

    Init commit

---
+++ src/Protobuf.CodeGen.BuildTask/ProtobufCodeGenTask.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace Protobuf.CodeGen.BuildTask
+{
+    public class ProtobufCodeGenTask : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public string ProtoCompilerToolPath { get; set; }
+
+        [Required]
+        public string[] ProtoPaths { get; set; }
+
+        [Required]
+        public string[] FileNames { get; set; }
+
+        [Required]
+        public string OutputDir { get; set; }
+
+        [Output]
+        public string StdOut { get; set; }
+
+        [Output]
+        public string StdError { get; set; }
+
+        public override bool Execute()
+        {
+            if (!(FileNames?.Length > 0))
+            {
+                return false;
+            }
+
+            var extendedProtoPath = new HashSet<string>(ProtoPaths, StringComparer.OrdinalIgnoreCase);
+            foreach (var file in FileNames)
+            {
+                extendedProtoPath.Add(Path.GetDirectoryName(file));
+            }
+
+            var argumentsBuilder = new StringBuilder();
+            foreach (var path in extendedProtoPath)
+            {
+                argumentsBuilder.Append($" -I {path}");
+            }
+
+            argumentsBuilder.Append($" --csharp_out={OutputDir}");
+
+            foreach (var file in FileNames)
+            {
+                argumentsBuilder.Append($" {file}");
+            }
+
+            if (!Directory.Exists(OutputDir))
+            {
+                Directory.CreateDirectory(OutputDir);
+            }
+
+            using (var p = Process.Start(new ProcessStartInfo
+            {
+                FileName = ProtoCompilerToolPath,
+                Arguments = argumentsBuilder.ToString(),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            }))
+            {
+                p.WaitForExit();
+
+                StdOut = p.StandardOutput.ReadToEnd();
+                StdError = p.StandardError.ReadToEnd();
+
+                if (!string.IsNullOrEmpty(StdOut))
+                {
+                    Log.LogError(StdOut);
+                }
+
+                if (!string.IsNullOrEmpty(StdError))
+                {
+                    Log.LogError(StdError);
+                }
+
+                return p.ExitCode == 0;
+            }
+        }
+    }
+}
